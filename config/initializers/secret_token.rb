28229a26a054cd09ea4b50d3b836fc3381003b7c
Tomas Nilsson <tomas.nilsson92@gmail.com>
2015-02-13 16:28:14 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpotiTraceServer::Application.config.secret_key_base = '0d3c60c381836d28aec8ba8dad5d57aad0df9f5bc96e37de86c39b42ca5b2636fa693ffed6f44cf4799099dfeb5ebf480d6be29bb24833a2019a7abcda8215f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpotiTraceServer::Application.config.session_store :cookie_store, key: '_SpotiTraceServer_session'
