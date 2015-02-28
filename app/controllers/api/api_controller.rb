b90fc31f1a7682c399f266faa89d99207bd8d941
Tomas Nilsson <tomas.nilsson92@gmail.com>
2015-02-28 14:46:33 -0800

    Secure the API

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,18 @@
 class Api::ApiController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_filter  :verify_authenticity_token # This should go into every Rails API controller
+  before_filter :restrict_access
+
+  private
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      @user = User.where(token: token).first
+    end
+  end
+
+  protected
+  def request_http_token_authentication(realm = "Application")
+    self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
+    render json: {error: "Invalid token"}, status: :unauthorized
+  end
 end
